070901aef6ab09190903a85ba15768868590939a
bender <kudin.dima@gmail.com>
2026-06-07 15:45:56 +0000

    Update theme fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { ServiceWrapper } from "@/components/ServiceWrapper";
 import Tag from "@/tag/Tag";
 import { getVisualEditScript } from "@/utils/visual-edit-script";
 import { Libre_Baskerville } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 
 
 
@@ -41,10 +42,10 @@ export const metadata: Metadata = {
   },
 };
 
-const libreBaskerville = Libre_Baskerville({
-  variable: "--font-libre-baskerville",
+
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
   subsets: ["latin"],
-  weight: ["400", "700"],
 });
 const inter = Inter({
   variable: "--font-inter",
@@ -59,7 +60,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <ServiceWrapper>
-        <body className={`${libreBaskerville.variable} ${inter.variable} antialiased`}>
+        <body className={`${dmSans.variable} ${inter.variable} antialiased`}>
           <Tag />
           {children}
           <script
